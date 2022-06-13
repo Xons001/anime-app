a07f0e650ff55a0af9999d367b97dd04776fea7e
Sean <sean.saez.f@gmail.com>
2022-06-13 17:50:41 +0200

    Se muestran imagenes y texto del archivo json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const api = 'https://kitsu.io/api/edge/trending/anime'
+
+  const [series, setSeries] = useState([])
+
+  useEffect(() => {
+    getSeries()
+  }, [])
+
+  function getSeries(){
+    fetch (api)
+    .then(res => res.json())
+    .then(response => {
+      const { data = [] } = response
+      setSeries(data)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Anime App
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mis series favoritas de anime
         </a>
       </header>
+      <section className='anime-wrapper'>
+        {
+          series.map(serie => {
+            return(
+              <div key={serie.id}>
+                <h1 width={'50%'}>{serie.attributes.titles.en}</h1>
+                <img src={serie.attributes.posterImage.small}></img>
+              </div>
+            )
+          })
+        }
+      </section>
     </div>
-  );
+    );
 }
 
 export default App;
